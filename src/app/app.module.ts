6d7fcbab0bc63d47df1926723a7adcdb808daf96
unknown <denes.zsolt.08@gmail.com>
2021-03-16 20:21:45 +0200

    smart-pubs

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { AngularFireModule } from '@angular/fire';
+import { environment } from 'src/environments/environment';
+import { AngularFirestoreModule } from "@angular/fire/firestore";
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
+    FormsModule,
+    ReactiveFormsModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    AngularFireModule.initializeApp(environment.firebase),
+    AngularFirestoreModule
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,12 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDe9c_eLiL7AGME3NcQsrSWcLKT-NOTzE0",
+    authDomain: "smarter-pubs.firebaseapp.com",
+    projectId: "smarter-pubs",
+    storageBucket: "smarter-pubs.appspot.com",
+    messagingSenderId: "315823748320",
+    appId: "1:315823748320:web:6f4689a2cd178f5e904c85",
+    measurementId: "G-0598GFE343"
+  }
 };
